6ee34cafb30fb863687804d9d6f5252a73de37f6
cweave <cweav3r@gmail.com>
2020-08-07 16:38:29 -0400

    add babel config. add carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import pet from '@frontendmasters/pet'
+import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
 
 class Details extends React.Component {
-	constructor(props) {
-		super(props);
+	state = { loading: true };
 
-		this.state = {
-			loading: true
-		}
+	componentDidMount() {
+		pet.animal(this.props.id).then(({ animal }) => {
+			this.setState({
+				name: animal.name,
+				animal: animal.type,
+				location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+				description: animal.description,
+				media: animal.photos,
+				breed: animal.breeds.primary,
+				loading: false,
+			});
+		}, console.error);
 	}
-	componentDidMount () {
-		pet.animal(this.props.id)
-			.then(({animal}) => {
-				this.setState({
-					name: animal.name,
-					animal: animal.type,
-					location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-					description: animal.description,
-					media: animal.photos,
-					breed: animal.breeds.primary,
-					loading: false
-				});
-			}, console.error);
-	}
-	render () {
+	render() {
 		if (this.state.loading) {
-			return 	<h1>loading ...</h1>;
+			return <h1>loading ...</h1>;
 		}
 
-		const { animal, breed, location, description, name } = this.state;
+		const { animal, breed, location, description, name, media } = this.state;
 
 		return (
 			<div className="details">
+				<Carousel media={media} />
 				<div>
 					<h1>{name}</h1>
 					<h2>{`${animal} - ${breed} - ${location}`}</h2>
@@ -39,7 +35,7 @@ class Details extends React.Component {
 					<p>{description}</p>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
